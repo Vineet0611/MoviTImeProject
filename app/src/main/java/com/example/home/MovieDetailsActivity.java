92239494acbe1cf269fd7706d4e5eb41bc8743b4
omkar <pendkalkar.omkar786@gmail.com>
2023-05-24 19:49:27 +0530

    movie details load done

--- app/src/main/java/com/example/home/MovieDetailsActivity.java
+++ app/src/main/java/com/example/home/MovieDetailsActivity.java
@@ -10,6 +10,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.Nullable;
@@ -18,6 +19,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -40,10 +42,11 @@ public class MovieDetailsActivity extends AppCompatActivity {
 
     private static final String TAG = "MovieDetailsActivity";
     Button btn;
-    TextView moviename, moviegenre;
+    TextView moviename, moviegenre, movieabout, movielanguage, moviequality, movieduration, moviereleased;
     String movie_name= "NA";
+    ImageView movietrailer, movieimg;
+    String movie_genre, movie_img, movie_id, movie_about, movie_languages, movie_quality, movie_released, movie_trailer, movie_duration;
     String url = "https://inundated-lenders.000webhostapp.com/api/moviedetails.php";
-    static JSONArray myJsonArray = new JSONArray();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +55,15 @@ public class MovieDetailsActivity extends AppCompatActivity {
 
         btn = findViewById(R.id.bookButton);
         moviename = findViewById(R.id.moviename);
-        moviename = findViewById(R.id.moviegenre);
+        moviegenre = findViewById(R.id.moviegenre);
+        movieabout = findViewById(R.id.movieabout);
+        movielanguage = findViewById(R.id.languages);
+        moviequality = findViewById(R.id.screenType);
+        moviereleased = findViewById(R.id.released);
+        movieduration = findViewById(R.id.duration);
+        movietrailer = findViewById(R.id.trailerlogo);
+        movieimg = findViewById(R.id.movieimg);
+
 
         if(getIntent().hasExtra("movie_name") ){
             movie_name = getIntent().getStringExtra("movie_name");
@@ -68,23 +79,41 @@ public class MovieDetailsActivity extends AppCompatActivity {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-//                        try {
-//                            JSONObject myJsonObj = new JSONObject(response);
+                        try {
+                            JSONArray list = new JSONArray(response);
+                            movie_id = list.getJSONObject(0).getString("movie_id");
+                            movie_genre = list.getJSONObject(0).getString("movie_genre");
+                            movie_trailer = list.getJSONObject(0).getString("movie_ytlink");
+                            movie_duration = list.getJSONObject(0).getString("movie_duration");
+                            movie_about = list.getJSONObject(0).getString("movie_about");
+                            movie_released = list.getJSONObject(0).getString("movie_release");
+                            movie_languages = list.getJSONObject(0).getString("movie_languages");
+                            movie_quality = list.getJSONObject(0).getString("movie_quality");
+                            movie_img = list.getJSONObject(0).getString("movie_image");
+//                            JSONObject myJsonObj = list.getJSONObject();
 //                            myJsonArray.put(myJsonObj);
 //                            Log.d("movie ", "myJsonObj created");
-//                            movie_genre = myJsonArray.getJSONObject(0).getString("movie_genre");
 //                            Log.d("movie ", "movie_genre done");
 //                            Toast.makeText(MovieDetailsActivity.this, response, Toast.LENGTH_LONG).show();
-                                moviegenre.setText(response);
-//                        } catch (JSONException e) {
-//                            throw new RuntimeException(e);
-//                        }
+                            moviegenre.setText(movie_genre);
+                            movieabout.setText(movie_about);
+                            movielanguage.setText(movie_languages);
+                            moviequality.setText(movie_quality);
+                            movieduration.setText(movie_duration);
+                            moviereleased.setText(movie_released);
+                            Glide.with(MovieDetailsActivity.this)
+                                    .load(movie_img)
+                                    .placeholder(R.drawable.no_image)
+                                    .into(movieimg);
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(MovieDetailsActivity.this, "" + error.getMessage(), Toast.LENGTH_LONG).show();
+                        error.printStackTrace();
                         Log.d("movie", "LogE " + error.getMessage());
 
                     }
@@ -94,7 +123,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String,String> params = new HashMap<>();
                 params.put("moviename", movie_name);
-                return super.getParams();
+                return params;
             }
         };
                 stringRequest.setRetryPolicy(new DefaultRetryPolicy(
